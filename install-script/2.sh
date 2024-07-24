9af34036736b6aae2e310c2cd6620f094de73db7
Matt Wallace <git@savagetech.com>
2024-07-24 14:50:39 -0600

    newgrp

--- install-script/2.sh
+++ install-script/2.sh
@@ -1,4 +1,3 @@
-
 # Add the NVIDIA Docker repository and install the NVIDIA container toolkit
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
@@ -7,12 +6,12 @@ sudo apt-get install -y nvidia-container-toolkit
 sudo apt install -y nvidia-docker2
 sudo systemctl restart docker
 sudo usermod -aG docker $USER
+newgrp docker
 
 # Prompt for logout and login to apply group changes
 if [[ -z "${KAMIWAZA_INSTALL_UNATTENDED}" ]]; then
     read -p "Please log out and log back in for the group changes to take effect. Run script 3.sh after that."
 else
-    echo "unattended mode, running 3.sh with su for the docker group..."
-    exec su - $USER -c "./3.sh"
+    echo "unattended mode, running 3.sh..."
+    ./3.sh
 fi
-
