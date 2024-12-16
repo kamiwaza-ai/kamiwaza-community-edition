51363862c749f59faecfeb066fe1200c1be4c454
Matt Wallace <git@savagetech.com>
2024-12-16 09:52:31 -0700

    update nvm version

--- install-script/1.sh
+++ install-script/1.sh
@@ -10,7 +10,7 @@ sudo apt install -y python3.10 python3.10-dev libpython3.10-dev python3.10-venv 
 
 
 # Install NVM and Node.js
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install 21
