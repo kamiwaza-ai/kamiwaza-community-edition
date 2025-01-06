b88e2c42054ecbb6b0f914f3d86f59955e475c5a
Matt Wallace <mwallace@gmail.com>
2025-01-06 07:52:20 -0700

    Update 3.sh

--- install-script/3.sh
+++ install-script/3.sh
@@ -5,7 +5,7 @@ docker run --rm --gpus all nvidia/cuda:12.4.1-runtime-ubuntu22.04 nvidia-smi
 mkdir kamiwaza
 cd kamiwaza
 wget https://github.com/kamiwaza-ai/kamiwaza-community-edition/blob/main/kamiwaza-community-0.3.2-UbuntuLinux.tar.gz
-tar -xvf kamiwaza-community-0.3.1-UbuntuLinux.tar.gz
+tar -xvf kamiwaza-community-0.3.2-UbuntuLinux.tar.gz
 bash install.sh
 
 echo "Kamiwaza installation completed. Please follow the post-installation steps to start the services and verify the installation."
